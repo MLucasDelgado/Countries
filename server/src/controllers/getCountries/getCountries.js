946ee278d3bd72ac43006f20e1d742b0d272d60e
MLucasDelgado <lucasdelgado1011@gmail.com>
2023-11-07 18:31:50 -0300

    ultimos detalles del back

--- server/src/controllers/getCountries/getCountries.js
+++ server/src/controllers/getCountries/getCountries.js
@@ -1,8 +1,8 @@
-const { Country } = require('../../db')
+const { Country, Activity } = require('../../db')
 
 const getCountries = async (req, res) => {
     try{  
-        const country = await Country.findAll()
+        const country = await Country.findAll({include: Activity })
         res.status(200).json(country)
     } catch (error) {
         res.status(500).send(error.message)
