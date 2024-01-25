9b3669d74ea79c751ed0a6af6c2c6167c20fb425
MLucasDelgado <lucasdelgado1011@gmail.com>
2024-01-25 15:45:11 -0300

    cambios en landing

--- client/src/pages/inicio/Inicio.jsx
+++ client/src/pages/inicio/Inicio.jsx
@@ -2,16 +2,20 @@ import Button from "../../components/button/button"
 import style from "./Inicio.module.css"
 
 const Inicio = () => {
-    return(
-        <div className={style.fondo}>
-            <section>
-            <h1 className={style.texto}>Countries</h1>
-            <h3 className={style.texto}>Conoce acerca de los paises de tus sueños</h3>
+    return (
+        <section className={style.seccion}>
+            <section className={style.contenido}>
+                <section>
+                    <h1 className={style.texto}>Countries</h1>
+                    <h3 className={style.descripcion}>Explore fascinating and diverse facts about countries around the world with our comprehensive collection of information.</h3>
+                </section>
+                <nav>
+                    <Button link={'/home'} text={'Log In'} className={style.boton} />
+                </nav>
             </section>
-            <nav>
-                <Button link={'/home'} text={'Home'} className={style.boton} />
-            </nav>
-        </div>
+            <div className={style.fondo}></div>
+        </section>
+
     )
 }
 
