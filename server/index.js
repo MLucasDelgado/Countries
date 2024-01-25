0fe6442cc630273463616baea553387cc5b13516
MLucasDelgado <lucasdelgado1011@gmail.com>
2024-01-25 18:02:35 -0300

    ultimos cambios

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const { getInformation } = require("./src/utils/conexion");
-const PORT = 3001;
+const PORT = 10000;
+const host = "0.0.0.0";
 
 conn.sync({ force: true }).then(() => {
-server.listen(PORT, async () => {
+server.listen(PORT, host, async () => {
   await getInformation()
   console.log(`Server listening on port ${PORT}`);
 })
